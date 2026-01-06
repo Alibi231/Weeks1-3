a4731487b559a203b69f6cd2fad87f48f7cef756
hammonmi <hammonmi@OA-S141-09>
2026-01-06 11:53:09 -0500

    Added the mouse tracking circle

---
+++ Assets/FollowMouse.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+
+public class FollowMouse : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        transform.position = mousePos;
+    }
+}
